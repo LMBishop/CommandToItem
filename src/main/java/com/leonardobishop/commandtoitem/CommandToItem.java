5b659dbd4570745a4e1ba4a953ac56b0b52608b2
LMBishop <13875753+LMBishop@users.noreply.github.com>
2020-04-13 03:16:41 +0100

    Fix enchantments to use namespace & all version compatibility

--- src/main/java/com/leonardobishop/commandtoitem/CommandToItem.java
+++ src/main/java/com/leonardobishop/commandtoitem/CommandToItem.java
@@ -4,6 +4,11 @@ import com.google.common.io.ByteStreams;
 import com.leonardobishop.commandtoitem.bstats.Metrics;
 import com.leonardobishop.commandtoitem.commands.BaseCommand;
 import com.leonardobishop.commandtoitem.events.UseItem;
+import com.leonardobishop.commandtoitem.utils.itemgetter.ItemGetter;
+import com.leonardobishop.commandtoitem.utils.itemgetter.ItemGetterLatest;
+import com.leonardobishop.commandtoitem.utils.itemgetter.ItemGetter_1_13;
+import com.leonardobishop.commandtoitem.utils.itemgetter.ItemGetter_Late_1_8;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,6 +22,7 @@ public class CommandToItem extends JavaPlugin {
 
     private List<Item> items = new ArrayList<>();
     private Metrics metrics;
+    private ItemGetter itemGetter;
 
     @Override
     public void onEnable() {
@@ -29,7 +35,6 @@ public class CommandToItem extends JavaPlugin {
         if (metrics.isEnabled()) {
             this.getLogger().log(Level.INFO, "Metrics started. This can be disabled at /plugins/bStats/config.yml.");
         }
-
 
         File config = new File(this.getDataFolder() + File.separator + "config.yml");
         if (!config.exists()) {
@@ -49,11 +54,14 @@ public class CommandToItem extends JavaPlugin {
                 super.getLogger().severe(ChatColor.RED + "...please delete the CommandToItem directory and try RESTARTING (not reloading).");
             }
         }
-        this.reloadConfig();
 
 
         super.getServer().getPluginCommand("commandtoitem").setExecutor(new BaseCommand(this));
         super.getServer().getPluginManager().registerEvents(new UseItem(this), this);
+
+        executeVersionSpecificActions();
+
+        this.reloadConfig();
     }
 
     public String getMessage(Message message) {
@@ -67,7 +75,7 @@ public class CommandToItem extends JavaPlugin {
 
         items.clear();
         for (String s : this.getConfig().getConfigurationSection("items").getKeys(false)) {
-            ItemStack is = Item.getItemStack("items." + s, this.getConfig());
+            ItemStack is = itemGetter.getItem("items." + s, this.getConfig(), this);
 
             // supports old config layout without "on-use"
             boolean consume = this.getConfig().getBoolean("items." + s + "on-use.consume",
@@ -89,13 +97,34 @@ public class CommandToItem extends JavaPlugin {
 
             items.add(new Item(s.replace(" ", "_"), is, commands, messages, consume, cooldown, sound));
         }
+
+
     }
 
     public List<Item> getItems() {
         return items;
     }
 
+    private void executeVersionSpecificActions() {
+        String version;
+        try {
+            version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            getLogger().warning("Failed to resolve server version - some features will not work!");
+            itemGetter = new ItemGetter_Late_1_8();
+            return;
+        }
 
+        getLogger().info("Your server is running version " + version + ".");
+        if (version.startsWith("v1_7") || version.startsWith("v1_8") || version.startsWith("v1_9")
+                || version.startsWith("v1_10") || version.startsWith("v1_11") || version.startsWith("v1_12")) {
+            itemGetter = new ItemGetter_Late_1_8();
+        } else if (version.startsWith("v1_13")) {
+            itemGetter = new ItemGetter_1_13();
+        } else {
+            itemGetter = new ItemGetterLatest();
+        }
+    }
 
     public enum Message {
 
--- src/main/java/com/leonardobishop/commandtoitem/Item.java
+++ src/main/java/com/leonardobishop/commandtoitem/Item.java
@@ -1,191 +1,21 @@
 package com.leonardobishop.commandtoitem;
 
 import com.leonardobishop.commandtoitem.utils.GSound;
+import com.leonardobishop.commandtoitem.utils.itemgetter.ItemGetter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class Item {
-
-    /*
-    item reader for 1.14+ ONLY
-
-    complete & working supporting:
-     - name
-     - material
-     - lore
-     - enchantments
-     - itemflags
-     - unbreakable
-     - attribute modifier
-     - custom model data
-     */
-    public static ItemStack getItemStack(String path, FileConfiguration config) {
-        String cName = config.getString(path + ".name", path + ".name");
-        String cType = config.getString(path + ".item", path + ".item");
-        boolean hasCustomModelData = config.contains(path + ".custommodeldata");
-        int customModelData = config.getInt(path + ".custommodeldata", 0);
-        boolean unbreakable = config.getBoolean(path + ".unbreakable", false);
-        List<String> cLore = config.getStringList(path + ".lore");
-        List<String> cItemFlags = config.getStringList(path + ".itemflags");
-        boolean hasAttributeModifiers = config.contains(path + ".attributemodifiers");
-        List<Map<?, ?>> cAttributeModifiers = config.getMapList(path + ".attributemodifiers");
-
-        String name;
-        Material type = null;
-        int data = 0;
-
-        // lore
-        List<String> lore = new ArrayList<>();
-        if (cLore != null) {
-            for (String s : cLore) {
-                lore.add(ChatColor.translateAlternateColorCodes('&', s));
-            }
-        }
-
-        // name
-        name = ChatColor.translateAlternateColorCodes('&', cName);
-
-        // material
-        type = Material.matchMaterial(cType);
-        if (type == null) {
-            type = Material.STONE;
-        }
-
-        ItemStack is = new ItemStack(type, 1, (short) data);
-        ItemMeta ism = is.getItemMeta();
-        ism.setLore(lore);
-        ism.setDisplayName(name);
-
-        // custom model data
-        if (hasCustomModelData) {
-            ism.setCustomModelData(customModelData);
-        }
-
-        // attribute modifiers
-        if (hasAttributeModifiers) {
-            for (Map<?, ?> attr : cAttributeModifiers) {
-                String cAttribute = (String) attr.get("attribute");
-                Attribute attribute = null;
-                for (Attribute enumattr : Attribute.values()) {
-                    if (enumattr.toString().equals(cAttribute)) {
-                        attribute = enumattr;
-                        break;
-                    }
-                }
-
-                if (attribute == null) continue;
-
-                Map<?, ?> configurationSection = (Map<?, ?>) attr.get("modifier");
-
-                String cUUID = (String) configurationSection.get("uuid");
-                String cModifierName = (String) configurationSection.get("name");
-                String cModifierOperation = (String) configurationSection.get("operation");
-                double cAmount;
-                try {
-                    Object cAmountObj = configurationSection.get("amount");
-                    if (cAmountObj instanceof Integer) {
-                        cAmount = ((Integer) cAmountObj).doubleValue();
-                    } else {
-                        cAmount = (Double) cAmountObj;
-                    }
-                } catch (Exception e) {
-                    cAmount = 1;
-                }
-                String cEquipmentSlot = (String) configurationSection.get("equipmentslot");
-
-                UUID uuid = null;
-                if (cUUID != null) {
-                    try {
-                        uuid = UUID.fromString(cUUID);
-                    } catch (Exception ignored) {
-                        // ignored
-                    }
-                }
-                EquipmentSlot equipmentSlot = null;
-                if (cEquipmentSlot != null) {
-                    try {
-                        equipmentSlot = EquipmentSlot.valueOf(cEquipmentSlot);
-                    } catch (Exception ignored) {
-                        // ignored
-                    }
-                }
-                AttributeModifier.Operation operation = AttributeModifier.Operation.ADD_NUMBER;
-                try {
-                    operation = AttributeModifier.Operation.valueOf(cModifierOperation);
-                } catch (Exception ignored) {
-                    // ignored
-                }
-
-                AttributeModifier modifier;
-                if (uuid == null) {
-                    modifier = new AttributeModifier(cModifierName, cAmount, operation);
-                } else if (equipmentSlot == null) {
-                    modifier = new AttributeModifier(uuid, cModifierName, cAmount, operation);
-                } else {
-                    modifier = new AttributeModifier(uuid, cModifierName, cAmount, operation, equipmentSlot);
-                }
-
-                ism.addAttributeModifier(attribute, modifier);
-            }
-        }
-
-        // item flags
-        if (config.isSet(path + ".itemflags")) {
-            for (String flag : cItemFlags) {
-                for (ItemFlag iflag : ItemFlag.values()) {
-                    if (iflag.toString().equals(flag)) {
-                        ism.addItemFlags(iflag);
-                        break;
-                    }
-                }
-            }
-        }
-
-
-        // unbreakable
-        ism.setUnbreakable(unbreakable);
-
-        // enchantments
-        if (config.isSet(path + ".enchantments")) {
-            for (String key : config.getStringList(path + ".enchantments")) {
-                String[] split = key.split(":");
-                String ench = split[0];
-                String level = null;
-                if (split.length == 2) {
-                    level = split[1];
-                } else {
-                    level = "1";
-                }
-
-                if (Enchantment.getByName(ench) == null) continue;
-
-                try {
-                    Integer.parseInt(level);
-                } catch (NumberFormatException e) {
-                    level = "1";
-                }
-
-                is.addUnsafeEnchantment(Enchantment.getByName(ench), Integer.parseInt(level));
-            }
-        }
-
-        is.setItemMeta(ism);
-        return is;
-    }
 
     private Map<UUID, Long> cooldowns = new HashMap<>();
 
